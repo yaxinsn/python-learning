e2e69e5211c5185b1b18050b0a311a4aa15cf069
yaxinsn <yaxinsn@163.com>
2017-11-18 19:37:52 -0800

    原始字符串，Unicode字符串，长字符串

--- code/print.py
+++ code/print.py
@@ -5,4 +5,11 @@ a= 10
 print '10//3 is ' + str(10//3) + '; '
 print '10//3 is ' + repr(10//3) + '; '
 print '10//3 is ' + `10//3` + '; '
+long_str=u''' I'm is python learner and I can print "Hello world!" 
+100//3 = 3
+'''
+print(long_str)
 
+print r"'c:\need_path'"
+print "'c:\need_path'"
+print r'Let\'s go'  # Let\' go
