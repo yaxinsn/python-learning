bd1c1c461b61384546b065d5c4ab015ce47922b6
yaxinsn <yaxinsn@163.com>
2017-11-19 19:44:30 -0800

    python的容器有三个大类，序列，字典，集合

---
+++ code/sequence/list_tuple_set.py
@@ -0,0 +1,20 @@
+
+
+
+
+tup=(1,2,3,1,2,3)
+print "tup is "
+print(tup)
+
+list1=[1,2,3,1,2,3]
+print "list is "
+print(list1)
+list1[0]= 10
+print "list can be modified "
+print(list1)
+
+
+set1=set(list1)
+print "set not has dup element, it delete dup element,automatically "
+print(set1)
+
