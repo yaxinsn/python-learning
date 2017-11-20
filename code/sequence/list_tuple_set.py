a4cb4d415fe32c91674568cb2b6f0b49b44834c3
yaxinsn <yaxinsn@163.com>
2017-11-20 03:28:08 -0800

    list 索引 与分片

--- code/sequence/list_tuple_set.py
+++ code/sequence/list_tuple_set.py
@@ -5,6 +5,7 @@
 tup=(1,2,3,1,2,3)
 print "tup is "
 print(tup)
+print(tup[2:4])
 
 list1=[1,2,3,1,2,3]
 print "list is "
@@ -17,4 +18,4 @@ print(list1)
 set1=set(list1)
 print "set not has dup element, it delete dup element,automatically "
 print(set1)
-
+#print(set1[2:3]) #is not support getitem
