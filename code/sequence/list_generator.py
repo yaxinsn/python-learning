6b9ee43a176a1e3dc9a8db97acd96dfad6d95b8c
yaxinsn <yaxinsn@163.com>
2017-12-01 02:45:30 -0800

    生成器，迭代器，容器

---
+++ code/sequence/list_generator.py
@@ -0,0 +1,23 @@
+#--coding:utf-8--
+##print 
+# str = reqr = `` 
+###############################################
+comment = u'''
+描述：列表生成器
+
+
+在Python中，这种一边循环一边计算的机制，称为生成器：generator。
+
+ L = [x * x for x in range(10)] 这是生成式。直接生成list.
+ g = (x * x for x in range(10)) 这就是生成器
+
+  next(g);
+  yield 语句。
+
+generator保存的是算法
+
+
+   '''
+   print(comment)
+
+   ###############################################
