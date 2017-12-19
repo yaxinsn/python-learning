849bb8e4203716ade3f529f8279e6a48435046d5
yaxinsn <yaxinsn@163.com>
2017-12-18 18:25:00 -0800

    type ininstance

---
+++ code/type_and_isinstance.py
@@ -0,0 +1,20 @@
+
+#--coding:utf-8--
+##print 
+# str = reqr = `` 
+###############################################
+comment = u'''
+描述：type(134)==int
+        type("sdfdsf")==str
+'''
+print(comment)
+
+###############################################
+
+
+a=124
+if type(a) == int :
+    print("a is int")
+
+if isinstance(a,int):
+    print("a is int")
